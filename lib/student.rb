c4775e423cbbf97c36264de9e809d789410400a1
Doris Morales <moralesdoris880@gmail.com>
2022-02-11 16:06:12 -0500

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+require_relative "./user"
+class Student < User
+    def initialize
+        @knowledge= []
+    end
+    def learn(knowledge_string)
+        @knowledge.push(knowledge_string)
+    end
+    def knowledge
+        @knowledge
+    end
 end
